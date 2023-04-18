bf614805515fae99fb81901fe379113b54f5fd51
alethinkion <91850089+alethinkion@users.noreply.github.com>
2023-04-18 19:46:42 -0300

    Update Pizarra.java

--- _scripts/Pizarra.java
+++ _scripts/Pizarra.java
@@ -1,14 +1,52 @@
+//TODO: agregar que usuario y anuncio sea una base de datos, en un txt, crear clases par manejar ada uno.
+//      Crear clase system que arranca todas las claes, deberia reemplazar al codigo de main.
+
 package _scripts;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Pizarra {
 
-    private ArrayList<Anuncio> allAnuncios = new ArrayList<Anuncio>();
+    private ArrayList<Anuncio> allAnuncios;
+
+    public Pizarra()
+    {
+        allAnuncios = new ArrayList<Anuncio>();
+    }
+
+    public void addAnuncio()
+    {
+        Scanner inputData = new Scanner (System.in);
+        String subjet;
+        String body;
+
+        System.out.println("Enter the subjet of anuncio");
+        subjet = inputData.next();
+
+        System.out.println("Enter the body of anuncio");
+        body = inputData.next();
+
+        Anuncio newAnuncio = new Anuncio( subjet, body );
+        allAnuncios.add( newAnuncio );
+    }
+
+    public void listAllSubjet( Pizarra currentpizarra )
+    {
+        allAnuncios.forEach( System.out::println );
+    }
 
     public static void main(String[] args)
     {
-        Anuncio anuncio = new Anuncio( "Test", "Esto es un cuerpo de mensaje" );
-        Anuncio anuncio1 = new Anuncio( "Test1", "Esto es un cuerpo de mensaje1" );
+        Pizarra pizarra = new Pizarra();
+        boolean isLoading = true;
+
+        System.out.println("");
+        while( isLoading )
+        {
+
+        }
+        pizarra.addAnuncio();
+
+        pizarra.listAllSubjet( pizarra );
     }
 }
